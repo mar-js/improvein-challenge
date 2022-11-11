86eeac652801dd314a05d81cb3cb1c8832ab3da8
Martin <maguera37@gmail.com>
2022-11-11 16:15:38 -0300

    feat: I added news interfaces

--- libs/assets/interfaces/index.ts
+++ libs/assets/interfaces/index.ts
@@ -4,8 +4,44 @@ export interface IChildren {
   children: React.ReactNode
 }
 
+export interface IAuth {
+  user: string | null;
+  token: string | null;
+}
+
+export interface IFilters {
+  id: number;
+  option: string;
+  value: string;
+}
+
+export interface IParams {
+  id: string;
+}
+
+export interface IBandP {
+  bands: IBand[]
+}
+
+export interface IBandD {
+  band: IBand
+}
+
 export interface INavigation {
   id: number;
   name: string;
   url: string;
+}
+
+export interface IBand {
+  id: number;
+  name: string;
+  genreCode: string;
+  year: number;
+  country: string;
+  members: IMember[];
+}
+
+export interface IMember {
+  name: string;
 }
