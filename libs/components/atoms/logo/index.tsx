a6fd061676cc2da284e7285e46a68daa760eaae7
Martin <maguera37@gmail.com>
2022-11-11 16:19:32 -0300

    feat: I created component in atoms

--- libs/components/atoms/logo/index.tsx
+++ libs/components/atoms/logo/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 export const Logo: React.FC = () => (
   <Link href="/">
     <Image
+      priority
       width={ 150 }
       height={ 50 }
       src="/improvein_logotipo.svg"
