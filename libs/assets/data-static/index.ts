8d279e0d2919d7b45db81b6d0e8d9a984aa41606
Martin <maguera37@gmail.com>
2022-11-11 00:13:07 -0300

    feat: I created folder assets and any folders

---
+++ libs/assets/data-static/index.ts
@@ -0,0 +1,14 @@
+import { INavigation } from '@assets/interfaces'
+
+export const NAVIGATION: INavigation[] = [
+  {
+    id: 1,
+    name: 'Sign In',
+    url: ''
+  },
+  {
+    id: 2,
+    name: 'Sign Up',
+    url: ''
+  }
+]
---
+++ libs/assets/interfaces/index.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export interface IChildren {
+  children: React.ReactNode
+}
+
+export interface INavigation {
+  id: number;
+  name: string;
+  url: string;
+}
