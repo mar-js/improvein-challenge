e70e99034416e761a8299e958ab85070dddf848e
Martin <maguera37@gmail.com>
2022-11-12 00:48:20 -0300

    feat: I added button back

--- libs/components/atoms/logo/index.tsx
+++ libs/components/atoms/logo/index.tsx
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export const Logo: React.FC = () => (
-  <Link href="/">
-    <Image
-      priority
-      width={ 150 }
-      height={ 50 }
-      src="/improvein_logotipo.svg"
-      alt="Logo Improve-in"
-    />
-  </Link>
-)
+export const Logo: React.FC = () => {
+  const { pathname } = useRouter()
+
+  return (
+    <Link href={ pathname !== '/' ? '/bands' : '/' }>
+      <Image
+        priority
+        width={ 150 }
+        height={ 50 }
+        src="/improvein_logotipo.svg"
+        alt="Logo Improve-in"
+      />
+    </Link>
+  )
+}
