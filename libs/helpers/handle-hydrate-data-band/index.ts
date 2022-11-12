af81ea1f2adde9e73ff649f2fad3bbd2405cd77a
Martin <maguera37@gmail.com>
2022-11-12 14:09:53 -0300

    fix: I correction filter by year

--- libs/helpers/handle-hydrate-data-band/index.ts
+++ libs/helpers/handle-hydrate-data-band/index.ts
@@ -4,7 +4,7 @@ export const handleHydrateDataBand = (obj: IBand[], objCopy: IBand[], order?: st
 
   if (order === 'asc') return objCopy.sort((a, b) => a.name.normalize().localeCompare(b.name.normalize()))
   if (order === 'des') return objCopy.sort((a, b) => b.name.normalize().localeCompare(a.name.normalize()))
-  if (order === 'year') return objCopy.sort((a, b) => a.year - b.year)
+  if (order === 'year') return objCopy.sort((a, b) => a.year + b.year)
 
   return obj
 }
