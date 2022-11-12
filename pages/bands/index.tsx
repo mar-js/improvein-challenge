fb55814672dc8b8c2c490569238f7932d8e617d0
Martin <maguera37@gmail.com>
2022-11-12 02:21:34 -0300

    fix: I correction page bands

--- pages/bands/index.tsx
+++ pages/bands/index.tsx
@@ -1,8 +1,14 @@
 import { Filters } from '@components/molecules'
 
 import { ContainerCardBand } from '@components/organisms'
+import { getBands } from 'services/get-bands'
+import { GetServerSideProps } from 'next'
+import { IBand } from '@assets/interfaces'
+import { RootState, WRAPPER } from 'redux/store'
+import { connect } from 'react-redux'
+import { get_bands } from 'redux/slices/bands'
 
-export default function Bands() {
+function Bands() {
   return (
     <>
       <Filters />
@@ -10,3 +16,15 @@ export default function Bands() {
     </>
   )
 }
+
+export const getStaticProps: GetServerSideProps = WRAPPER.getServerSideProps(store => async () => {
+  const BANDS = await getBands('bands')
+
+  store.dispatch(get_bands((BANDS as IBand[])))
+
+  return { props: { } }
+})
+
+const mapStateToProps = (state: RootState) => ({ bands: state.bands })
+
+export default connect(mapStateToProps)(Bands)
