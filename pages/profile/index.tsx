c94ac45e71bec0043604fd399b86ef2162ec7036
Martin <maguera37@gmail.com>
2022-11-12 00:52:10 -0300

    feat: I created page for the profile

---
+++ pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import { ProfileCard } from '@components/molecules'
+import { Section } from 'layouts/section'
+
+export default function Profile() {
+  return (
+    <Section>
+      <ProfileCard />
+    </Section>
+  )
+}
