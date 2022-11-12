709a0127560bcb3d2172916308552ff8da0a8e6f
Martin <maguera37@gmail.com>
2022-11-12 00:51:51 -0300

    fix: I correction error in fetch data of the bands

--- pages/bands/index.tsx
+++ pages/bands/index.tsx
@@ -1,30 +1,12 @@
 import { Filters } from '@components/molecules'
 
-import { getBands } from 'services/get-bands'
 import { ContainerCardBand } from '@components/organisms'
-import { GetServerSideProps } from 'next'
-import { IBand, IBandP } from '@assets/interfaces'
-import { RootState, WRAPPER } from 'redux/store'
-import { connect } from 'react-redux'
-import { get_bands } from 'redux/slices/bands'
 
-function Bands({ bands }: IBandP) {
+export default function Bands() {
   return (
     <>
       <Filters />
-      <ContainerCardBand bands={ bands } />
+      <ContainerCardBand />
     </>
   )
 }
-
-export const getServerSideProps: GetServerSideProps = WRAPPER.getServerSideProps(store => async () => {
-  const BANDS = await getBands('bands')
-
-  store.dispatch(get_bands((BANDS as IBand[])))
-
-  return { props: { } }
-})
-
-const mapStateToProps = (state: RootState) => ({ bands: state.bands })
-
-export default connect(mapStateToProps)(Bands)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return 'aaa'
+import { FormLogin } from '@components/molecules'
+import { Section } from 'layouts/section'
+import { getBands } from 'services/get-bands'
+import { GetServerSideProps } from 'next'
+import { IBand } from '@assets/interfaces'
+import { RootState, WRAPPER } from 'redux/store'
+import { connect } from 'react-redux'
+import { get_bands } from 'redux/slices/bands'
+
+function Home() {
+  return (
+    <Section>
+      <FormLogin />
+    </Section>
+  )
 }
+
+export const getStaticProps: GetServerSideProps = WRAPPER.getServerSideProps(store => async () => {
+  const BANDS = await getBands('bands')
+
+  store.dispatch(get_bands((BANDS as IBand[])))
+
+  return { props: { } }
+})
+
+const mapStateToProps = (state: RootState) => ({ bands: state.bands })
+
+export default connect(mapStateToProps)(Home)
