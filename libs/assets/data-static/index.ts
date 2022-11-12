672f445705076c27580ca0e780d87532e3dbc1ab
Martin <maguera37@gmail.com>
2022-11-12 00:50:35 -0300

    fix: I correction the data static for the navigation

--- libs/assets/data-static/index.ts
+++ libs/assets/data-static/index.ts
@@ -3,13 +3,8 @@ import { IFilters, INavigation } from '@assets/interfaces'
 export const NAVIGATION: INavigation[] = [
   {
     id: 1,
-    name: 'Sign In',
-    url: ''
-  },
-  {
-    id: 2,
-    name: 'Sign Up',
-    url: ''
+    name: 'Profile',
+    url: '/profile'
   }
 ]
 
